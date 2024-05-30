8677c6ccb3765129c27e1ab72c5f5099c11304a6
Andros924 <sir_vivi@hotmail.it>
2024-05-30 18:51:12 +0200

    primocommit

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+    return (
+      <section className="about">
+        <div className="container">
+          <h2>Chi Sono</h2>
+          <Bio />
+          <h2>Servizi</h2>
+          <Services />
+        </div>
+      </section>
+    );
+  };
+  
+  export default About;
---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactInfo = () => {
+  return (
+    <div className="contact-info">
+      <p>Telefono: 3890536285</p>
+      <p>Email: sturdiotrib.amoroso@gmail.com</p>
+      <p>Indirizzo: Via villa Rosato, 28 Palermo</p>
+    </div>
+  );
+};
+
+export default ContactInfo;
