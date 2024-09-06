674961f3088b3d7b9591247a054f12c36dad9179
Andros924 <sir_vivi@hotmail.it>
2024-09-06 20:56:56 +0200

    alcune modifiche

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
   theme: {
     extend: {
       colors: {
-        darkred: '#8B0000',  
+        darkred: '#8B0000',
         lightred: '#FF6347',
       },
     },
   },
-  plugins: [],
+  plugins: [], 
 }
